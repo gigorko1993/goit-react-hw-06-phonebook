11ee34e657dfc9e6599b1aa758afdf4d63e0e334
Igor Mokhnatskyy <55652900+gigorko1993@users.noreply.github.com>
2021-09-08 15:57:17 +0300

    fix bags

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactsReducer from "./contacts/contacts-reducer";
-// const initialState = {
-//   contacts: {
-//     items: [],
-//     filter: "",
-//     isLoading: false,
-//   },
-// };
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
   },
-  //   initialState,
   devTools: process.env.NODE_ENV === "development",
 });
